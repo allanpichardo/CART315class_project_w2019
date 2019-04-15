203788971f613beba9b07c5a0adf2a0c4fd3bf35
Ahipsagh <31903660+Ahipsagh@users.noreply.github.com>
2019-04-14 22:55:27 -0400

    Add sound and random Script
    Add sound and random Script

---
+++ CART315ClassProjectWinter2019/Assets/BubbleIslandTeam/scripts/RandomSounds.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSounds : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitAndPrint(Random.Range(4.0f, 10.0f))); 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+    // every 2 seconds perform the print()
+    private IEnumerator WaitAndPrint(float waitTime)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(waitTime);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
